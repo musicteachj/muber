c2c734b971fb690a8dec90738604458f26c94a8d
musicteachj <jimilittle3@hotmail.com>
2017-09-07 16:02:34 -0400

    update
    add more dependencies, files

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 const app = express();
 
-// Watch for incoming requests of method GET
-// to the route http://localhost:3050/api
-app.get('/api', (req, res) => {
-	res.send({ hi: 'there' });
-});
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/muber');
+
+// MUST BE ABOVE ROUTES(APP)
+app.use(bodyParser.json());
+routes(app);
 
 module.exports = app;
